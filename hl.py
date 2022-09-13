ffdbe5fdf36fe0890b5640de446048c97a03345f
Abhi-gltich <72931473+Abhi-gltich@users.noreply.github.com>
2022-09-13 13:17:33 +0545

    Update hl.py

--- hl.py
+++ hl.py
@@ -21,20 +21,20 @@ def compare(follower_count1, follower_count2):
 def game():
     play = True
     total = 0
+    print(logo)
+    account2 = generate_random_acc()
     while play:
-        print(logo)
-
-        account1 = generate_random_acc()
-        account2 = generate_random_acc()
+        account1 = account2
+        
         while account1 == account2:
             account2 = generate_random_acc()
 
         follower_count1 = account1["follower_count"]
         follower_count2 = account2["follower_count"]
 
-        print(manage_data(account1))
+        print( "Compare A:",manage_data(account1))
         print(vs)
-        print(manage_data(account2))
+        print("With B:",manage_data(account2))
 
         user_guess = input("Who has more followers? Press A or B. \n").lower()
         answer = compare(follower_count1, follower_count2)
@@ -46,6 +46,6 @@ def game():
         elif answer != user_guess:
             print(f"Wrong, you lose\nYour total is {total}")
             play = False
-
+        
 
 game()
